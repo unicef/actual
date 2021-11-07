09b48853551842da9ba8e8533a209f00ec8c98c4
Max Egorov <max@actualhq.com>
2021-11-07 11:06:12 -0800

    add census parameter wrapping

--- giga/utils/parameters.py
+++ giga/utils/parameters.py
@@ -36,5 +36,65 @@ class GigaParameters:
             json.dump(serialize_params(self.params), f, indent=4)
 
     @property
+    def emis(self):
+        return self.table_params['emis']
+
+    @property
+    def portal(self):
+        return self.table_params['portal']
+
+    @property
+    def fixed_bandwidth_rate(self):
+        return self.named_params['Fixed Bandwidth Rate']
+
+    @property
     def consolidation_radius(self):
         return self.named_params['School Consolidation Radius']
+
+    @property
+    def school_use_radius(self):
+        return self.named_params['School Use Radius']
+
+    @property
+    def internet_use_radius(self):
+        return self.named_params['Internet Use Radius']
+
+    @property
+    def school_age_fraction(self):
+        return self.named_params['School Age Fraction']
+
+    @property
+    def school_enrollment_fraction(self):
+        return self.named_params['School Enrollment Fraction']
+
+    @property
+    def student_teacher_ratio(self):
+        return self.named_params['Student Teacher Ratio']
+
+    @property
+    def teacher_classroom_ratio(self):
+        return self.named_params['Teacher Classroom Ratio']
+
+    @property
+    def people_per_household(self):
+        return self.named_params['People per Household']
+
+    @property
+    def emis_allowable_transfer_time(self):
+        return self.named_params['EMIS Allowable Transfer Time']
+
+    @property
+    def peak_hours(self):
+        return self.named_params['Peak Hours']
+
+    @property
+    def internet_browsing_bandwidth(self):
+        return self.named_params['Internet Browsing Bandwidth']
+
+    @property
+    def allowable_website_loading_time(self):
+        return self.named_params['Allowable Website Loading Time']
+
+    @property
+    def contention(self):
+        return self.named_params['Contention']
