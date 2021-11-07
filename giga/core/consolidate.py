40116aa0cb213c68487553f15aa2501b6c7939f9
Max Egorov <max@actualhq.com>
2021-11-06 22:31:12 -0700

    update parameter input for consolidation node

--- giga/core/consolidate.py
+++ giga/core/consolidate.py
@@ -7,36 +7,36 @@ DEFAULT_CONSOLIDATION_RADIUS = 0.01 # km
 
 class ConsolidationNode:
 
-	def __init__(self, name, **kwargs):
-		self.name = name
-		# optional position keys
-		self.consolidation_radius = kwargs.get('consolidation_radius', DEFAULT_CONSOLIDATION_RADIUS)
-		self.pt_input = kwargs.get('pt_input', ['Lat', 'Lon'])
-		self.dim = len(self.pt_input)
+    def __init__(self, name, **kwargs):
+        self.name = name
+        # optional position keys
+        self.consolidation_radius = kwargs.get('consolidation_radius', DEFAULT_CONSOLIDATION_RADIUS)
+        self.location_input = kwargs.get('location_input', ['Lat', 'Lon'])
+        self.dim = len(self.location_input)
 
-	def consolidate(self, data, radius):
-		# consolidates data based on its location in self.pt_input
-		# returns a consolidated subset of data where data points with location within specified radius have been filtered out
-		radius *= DEGREES_PER_1KM # trasnform to degrees
-		identifiers = set()
-		consolidated = []
-		pts = data[self.pt_input].to_numpy()
-		index = GriSPy(pts)
-		for i, datarow in data.iterrows():
-			pt = datarow[self.pt_input]
-			pt = pt.to_numpy(dtype=float).reshape(1, self.dim)
-			dists, idxs = index.bubble_neighbors(pt, distance_upper_bound=radius, sorted=True)
-			idxs = idxs[0]
-			if i in identifiers:
-				consolidated.append(False)
-			else:
-				consolidated.append(True)
-				for ii in idxs[1:]:
-					identifiers.add(ii)
-		return consolidated
+    def consolidate(self, data, radius):
+        # consolidates data based on its location in self.location_input
+        # returns a consolidated subset of data where data points with location within specified radius have been filtered out
+        radius *= DEGREES_PER_1KM # trasnform to degrees
+        identifiers = set()
+        consolidated = []
+        pts = data[self.location_input].to_numpy()
+        index = GriSPy(pts)
+        for i, datarow in data.iterrows():
+            pt = datarow[self.location_input]
+            pt = pt.to_numpy(dtype=float).reshape(1, self.dim)
+            dists, idxs = index.bubble_neighbors(pt, distance_upper_bound=radius, sorted=True)
+            idxs = idxs[0]
+            if i in identifiers:
+                consolidated.append(False)
+            else:
+                consolidated.append(True)
+                for ii in idxs[1:]:
+                    identifiers.add(ii)
+        return consolidated
 
-	def run(self, data, parameters):
-		radius = parameters.get('consolidation_radius', self.consolidation_radius)
-		consolidated = self.consolidate(data, radius)
-		return consolidated
+    def run(self, data, parameters):
+        radius = parameters.get('consolidation_radius', self.consolidation_radius)
+        consolidated = self.consolidate(data, radius)
+        return consolidated
 
