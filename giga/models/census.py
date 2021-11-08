38824c0dc225d24215013d0f399764456d9494a7
Max Egorov <max@actualhq.com>
2021-11-07 22:54:09 -0800

    census model arguments passing updates

--- giga/models/census.py
+++ giga/models/census.py
@@ -9,52 +9,44 @@ from giga.core.classroom import ClassroomEstimateNode
 from giga.utils.logging import LOGGER
 
 
-DEFAULT_SCHOOL_USE_RADIUS = 10.0 # km
-DEFAULT_INTERNET_USE_RADIUS = 1.0 # km
-DEFAULT_LOCATION_INPUT = ['Lat', 'Lon']
+STUDENT_VAR = 'num_students'
+TEACHER_VAR = 'num_teachers'
 SCHOOL_USE_POP_VAR = 'school_use_population'
 SCHOOL_USE_NERBY_COUNTS_VAR = 'school_use_nearby_counts'
+
+REQUIRED_ARGUMENTS = ('population_file', 'school_age_fraction', 'school_enrollment_fraction',
+                      'student_teacher_ratio', 'teacher_classroom_ratio', 'people_per_household')
+OPTIONAL_ARGUMENTS = {'location_input': ['Lat', 'Lon'],
+                      'school_use_radius': 10.0, # km
+                      'internet_use_radius': 1.0, # km
+                      }
 
 
 class CensusNode:
 
-    def __init__(self, population_file,
-                       school_age_fraction,
-                       school_enrollment_fraction,
-                       student_teacher_ratio,
-                       teacher_classroom_ratio,
-                       people_per_household,
-                       **kwargs):
-        self.location_input = kwargs.get('location_input', DEFAULT_LOCATION_INPUT)
-        self.people_per_household = people_per_household
-        self.school_use_radius = kwargs.get('school_use_radius', DEFAULT_SCHOOL_USE_RADIUS)
-        self.internet_use_radius = kwargs.get('internet_use_radius', DEFAULT_INTERNET_USE_RADIUS)
-        self.compute_students = kwargs.get('compute_students', True)
-        self.compute_teachers = kwargs.get('compute_teachers', True)
-        # output vars
-        self.student_var = kwargs.get('student_output', 'num_students')
-        self.teacher_var = kwargs.get('teacher_output', 'num_teachers')
-        self.classroom_var = kwargs.get('classroom_output', 'num_classrooms')
-        self.population_var = kwargs.get('population_output', 'nearby_population')
-        self.households_var = kwargs.get('households_output', 'nearby_households')
+    def __init__(self, name, **kwargs):
+        self.name = name
+        assert all([k in kwargs for k in REQUIRED_ARGUMENTS]), f"Missing one or more required arguments {REQUIRED_ARGUMENTS}"
+        for attr in REQUIRED_ARGUMENTS:
+            setattr(self, attr, kwargs.get(attr))
+        for attr in OPTIONAL_ARGUMENTS.keys():
+            setattr(self, attr, kwargs.get(attr, OPTIONAL_ARGUMENTS[attr]))
         # initialize compute nodes needed to perform a full census
-        self.population_node = PopulationNode.from_tiff('population-node', population_file,
+        self.population_node = PopulationNode.from_tiff('population-node', self.population_file,
                                                          location_input=self.location_input)
-
         self.nearest_neighbors_node = NearestNeighborsNode('nearest-neighbor-node',
                                                            np.zeros((1, len(self.location_input))), # default input
                                                            location_input=self.location_input)
         self.student_node = StudentEstimateNode('student-node',
-                                                school_age_fraction,
-                                                school_enrollment_fraction,
                                                 population_input=SCHOOL_USE_POP_VAR,
-                                                school_input=SCHOOL_USE_NERBY_COUNTS_VAR)
+                                                school_input=SCHOOL_USE_NERBY_COUNTS_VAR,
+                                                **kwargs)
         self.teacher_node = TeacherEstimateNode('teacher-node',
-                                                student_teacher_ratio,
-                                                student_input=self.student_var)
+                                                student_input=STUDENT_VAR,
+                                                **kwargs)
         self.classroom_node = ClassroomEstimateNode('classroom-node',
-                                                    teacher_classroom_ratio,
-                                                    teacher_input=self.teacher_var)
+                                                    teacher_input=TEACHER_VAR,
+                                                    **kwargs)
 
     def update_nodes(self, data, params):
         # dynamically update nodes based on input data
@@ -80,13 +72,13 @@ class CensusNode:
         return num_students
 
     def teacher_estimate(self, num_students, params):
-        data = pd.DataFrame(list(num_students), columns =[self.student_var])
+        data = pd.DataFrame(list(num_students), columns =[STUDENT_VAR])
         num_teachers = self.teacher_node.run(data, params)
         LOGGER.info("Completed teacher estimate")
         return num_teachers
 
     def classroom_estiamte(self, num_teachers, params):
-        data = pd.DataFrame(list(num_teachers), columns =[self.teacher_var])
+        data = pd.DataFrame(list(num_teachers), columns =[TEACHER_VAR])
         num_classrooms = self.classroom_node.run(data, params)
         LOGGER.info("Completed classroom estimate")
         return num_classrooms
