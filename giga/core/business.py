954572db516b15940c10edf66c3fbe3a7fccc102
Max Egorov <max@actualhq.com>
2021-11-07 21:17:19 -0800

    update inputs args parsing in business model

--- giga/core/business.py
+++ giga/core/business.py
@@ -1,16 +1,19 @@
 
 
+REQUIRED_ARGUMENTS = ('revenue_over_cost_factor',)
+OPTIONAL_ARGUMENTS = {'revenue_input': 'annual_revenue',
+                      'cost_input': 'annual_cost'}
+
+
 class BusinessModelNode:
 
-    def __init__(self,
-                 name,
-                 revenue_over_cost_factor,
-                 **kwargs):
+    def __init__(self, name, **kwargs):
         self.name = name
-        self.revenue_over_cost_factor = revenue_over_cost_factor
-        # optional params
-        self.revenue_input = kwargs.get('revenue_input', 'annual_revenue')
-        self.cost_input = kwargs.get('cost_input', 'annual_cost')
+        assert all([k in kwargs for k in REQUIRED_ARGUMENTS]), f"Missing one or more required arguments {REQUIRED_ARGUMENTS}"
+        for attr in REQUIRED_ARGUMENTS:
+            setattr(self, attr, kwargs.get(attr))
+        for attr in OPTIONAL_ARGUMENTS.keys():
+            setattr(self, attr, kwargs.get(attr, OPTIONAL_ARGUMENTS[attr]))
 
     def assess_business_model(self, datarow):
         if datarow[self.revenue_input] * self.revenue_over_cost_factor > datarow[self.cost_input]:
