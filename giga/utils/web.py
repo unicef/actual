8f8318bc6ea887879b4996dc40d05286e2f9e974
Max Egorov <max@actualhq.com>
2021-11-08 20:57:57 -0800

    workspaces fetch country borders

--- giga/utils/web.py
+++ giga/utils/web.py
@@ -6,9 +6,12 @@ BLOCK_SIZE = 1024 #1 Kibibyte
 
 DEFAULT_RWANDA_POPULATION_URL = 'https://data.worldpop.org/GIS/Population/Global_2000_2020/2020/RWA/rwa_ppp_2020_UNadj.tif'
 
+DEFAULT_BORDER_DATASET_URL = 'https://thematicmapping.org/downloads/TM_WORLD_BORDERS-0.3.zip'
+
 
 def get_file_with_progress(url, target_file, name='file'):
-    response = requests.get(url, stream=True)
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"}
+    response = requests.get(url, stream=True, headers=headers)
     total_size_in_bytes = int(response.headers.get('content-length', 0))
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
     progress_bar.set_description(f"Downloading {name}")
@@ -21,3 +24,6 @@ def get_file_with_progress(url, target_file, name='file'):
 
 def get_default_rwanda_pop_data(target_file):
     get_file_with_progress(DEFAULT_RWANDA_POPULATION_URL, target_file, 'Rwanda population data')
+
+def get_border_dataset(target_file):
+    get_file_with_progress(DEFAULT_BORDER_DATASET_URL, target_file, 'Border dataset')
--- giga/utils/workspace.py
+++ giga/utils/workspace.py
@@ -1,24 +1,48 @@
 import os
+import zipfile
+import tempfile
+import shutil
 from pathlib import Path
+import geopandas as gpd
 
 from giga.utils.parameters import GigaParameters
 from giga.utils.parse import DEFAULT_DOCID
-from giga.utils.web import get_default_rwanda_pop_data
+from giga.utils.web import get_default_rwanda_pop_data, get_border_dataset
 from giga.utils.logging import LOGGER
 
 
 POPULATION_DATASETS = {'Rwanda': get_default_rwanda_pop_data}
+FIPS_COUNTRY_CODES = {'Rwanda': 'RW'}
+SHAPEFILE_NAME = 'TM_WORLD_BORDERS-0.3.shp'
 
+
+def get_country_border_data(country, target_file):
+    assert country in FIPS_COUNTRY_CODES, f"Country {country} is not supported try one of {list(FIPS_COUNTRY_CODES.keys())}"
+    # download to a tmp directory
+    tmp = tempfile.mkdtemp()
+    tmpzip = os.path.join(tmp, 'borders.zip')
+    get_border_dataset(tmpzip)
+    with zipfile.ZipFile(tmpzip, "r") as zip_ref:
+        zip_ref.extractall(tmp)
+    # load in file and write to target
+    shapefile = os.path.join(tmp, SHAPEFILE_NAME)
+    borders = gpd.read_file(shapefile)
+    border = borders[borders['FIPS'] == FIPS_COUNTRY_CODES[country]]
+    border.to_file(target_file, driver='ESRI Shapefile')
+    #cleanup
+    shutil.rmtree(tmp)
 
 def create_workspace(dir, country, docid=DEFAULT_DOCID):
     assert country in POPULATION_DATASETS, f"Country {country} is not supported, try one of {list(POPULATION_DATASETS.keys())}"
     Path(dir).mkdir(parents=True, exist_ok=True)
     population_file = os.path.join(dir, 'population.tiff')
     config_file = os.path.join(dir, 'parameters.json')
-    get_default_rwanda_pop_data(population_file)
+    border_file = os.path.join(dir, 'border.shp')
+    POPULATION_DATASETS[country](population_file)
+    get_country_border_data(country, border_file)
     LOGGER.info(f'Fetching parameters from google sheet with ID {docid}')
     parameters = GigaParameters.from_google_sheet(docid)
     parameters.to_json(config_file)
-    return config_file, population_file
+    return config_file, population_file, border_file
 
 
