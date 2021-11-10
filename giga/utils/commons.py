cbb33f2efaadc5c67a0385dace72641287df0c9b
Max Egorov <max@actualhq.com>
2021-11-09 21:43:33 -0800

    add workspace support for other countries

--- giga/utils/commons.py
+++ giga/utils/commons.py
@@ -2,3 +2,14 @@
 
 DIRECT_STUDENT_INPUTS = ['num_students', 'NUMBER OF STUDENTS']
 DIRECT_TEACHER_INPUTS = ['NUMBER OF TEACHERS']
+
+DEFAULT_DOCID = "1LsOLtcZG8FO9uF79H7Z_PdN6iHkuMyr5TXw3UllbahE"
+
+SUPPORTED_COUNTRIES = {'Rwanda': {'FIPS': 'RW',
+                                  'population': 'https://data.worldpop.org/GIS/Population/Global_2000_2020/2020/RWA/rwa_ppp_2020.tif'},
+                       'Honduras': {'FIPS': 'HO',
+                                    'population': 'https://data.worldpop.org/GIS/Population/Global_2000_2020/2020/HND/hnd_ppp_2020.tif'},
+                       'Kenya': {'FIPS': 'KE',
+                                 'population': 'https://data.worldpop.org/GIS/Population/Global_2000_2020/2020/KEN/ken_ppp_2020.tif'},
+                       'Sierra Leone': {'FIPS': 'SL',
+                                        'population': 'https://data.worldpop.org/GIS/Population/Global_2000_2020/2020/SLE/sle_ppp_2020.tif'}}
--- giga/utils/workspace.py
+++ giga/utils/workspace.py
@@ -7,17 +7,16 @@ import geopandas as gpd
 
 from giga.utils.parameters import GigaParameters
 from giga.utils.parse import DEFAULT_DOCID
-from giga.utils.web import get_default_rwanda_pop_data, get_border_dataset
+from giga.utils.web import get_border_dataset, get_file_with_progress
 from giga.utils.logging import LOGGER
+from giga.utils.commons import SUPPORTED_COUNTRIES
 
 
-POPULATION_DATASETS = {'Rwanda': get_default_rwanda_pop_data}
-FIPS_COUNTRY_CODES = {'Rwanda': 'RW'}
 SHAPEFILE_NAME = 'TM_WORLD_BORDERS-0.3.shp'
 
 
 def get_country_border_data(country, target_file):
-    assert country in FIPS_COUNTRY_CODES, f"Country {country} is not supported try one of {list(FIPS_COUNTRY_CODES.keys())}"
+    assert country in SUPPORTED_COUNTRIES, f"Country {country} is not supported try one of {list(SUPPORTED_COUNTRIES.keys())}"
     # download to a tmp directory
     tmp = tempfile.mkdtemp()
     tmpzip = os.path.join(tmp, 'borders.zip')
@@ -27,18 +26,22 @@ def get_country_border_data(country, target_file):
     # load in file and write to target
     shapefile = os.path.join(tmp, SHAPEFILE_NAME)
     borders = gpd.read_file(shapefile)
-    border = borders[borders['FIPS'] == FIPS_COUNTRY_CODES[country]]
+    border = borders[borders['FIPS'] == SUPPORTED_COUNTRIES[country]['FIPS']]
     border.to_file(target_file, driver='ESRI Shapefile')
     #cleanup
     shutil.rmtree(tmp)
 
 def create_workspace(dir, country, docid=DEFAULT_DOCID):
-    assert country in POPULATION_DATASETS, f"Country {country} is not supported, try one of {list(POPULATION_DATASETS.keys())}"
+    assert country in SUPPORTED_COUNTRIES, f"Country {country} is not supported, try one of {list(SUPPORTED_COUNTRIES.keys())}"
+    # setup
     Path(dir).mkdir(parents=True, exist_ok=True)
     population_file = os.path.join(dir, 'population.tiff')
     config_file = os.path.join(dir, 'parameters.json')
     border_file = os.path.join(dir, 'border.shp')
-    POPULATION_DATASETS[country](population_file)
+    # fetch population
+    popurl = SUPPORTED_COUNTRIES[country]['population']
+    get_file_with_progress(popurl, population_file, f'{country} population data')
+    # fetch border data
     get_country_border_data(country, border_file)
     LOGGER.info(f'Fetching parameters from google sheet with ID {docid}')
     parameters = GigaParameters.from_google_sheet(docid)
