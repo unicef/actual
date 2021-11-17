5c5305d847f923da3f714e16a71ab85f95293900
Max Egorov <max@actualhq.com>
2021-11-17 11:46:43 -0800

    update test config path

--- test/test_giga.py
+++ test/test_giga.py
@@ -5,8 +5,6 @@ from giga.models.giga import GigaNode, REQUIRED_ARGUMENTS
 from giga.utils.parameters import GigaParameters
 
 
-#DEFAULT_CONFIG = 'defaults.json'
-
 DEFAULT_CONFIG = Path(__file__).resolve().parent / 'defaults.json'
 
 
@@ -19,5 +17,4 @@ def test_giga_model(giga_params):
 
 @pytest.fixture
 def giga_params():
-    print(DEFAULT_CONFIG)
     return GigaParameters.from_json(DEFAULT_CONFIG)
